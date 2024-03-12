0fa20464cd3b171345462d61e6f4945679200ba8
School-123 <160301423+School-123@users.noreply.github.com>
2024-03-12 17:12:17 +0530

    QRApp

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QRApp(),
+  ));
+}
+
+class QRApp extends StatefulWidget {
+  const QRApp({Key? key}) : super(key: key);
+
+  @override
+  State<QRApp> createState() => _QRAppState();
+}
+
+class _QRAppState extends State<QRApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+            'Login'
+            ),
+        SizedBox(
+          height: 40,
+          width: 18,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TextField(decoration: InputDecoration(hintText: 'enter your password',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  width: 3, color: Colors.greenAccent),
+              //<-- SEE HERE
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+          ),
+        ),
+        SizedBox(
+          height: 40,
+          width: 18,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TextField(decoration: InputDecoration(hintText: 'enter your password',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  width: 3, color: Colors.greenAccent),
+              //<-- SEE HERE
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+              ),
+
+            ),
+          ),
+
+        SizedBox(
+          height: 10,
+          width: 18,
+        ),
+        ElevatedButton(onPressed: () {},style: ElevatedButton.styleFrom(primary: Colors.green), child: Text('Login')),
+        Text(
+            textAlign: TextAlign.center,
+            'dont have an account?,sign in'),
+
+      ],
+    ),
+    );
+  }
+}
+
+
+
+
---
+++ lib/registration.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class registration extends StatefulWidget {
+  const registration({Key? key}) : super(key: key);
+
+  @override
+  State<registration> createState() => _registrationState();
+}
+
+class _registrationState extends State<registration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+              'Registration'),
+          SizedBox(
+            height: 40,
+            width: 18,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                labelText: 'enter your name',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            width: 18,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: 'enter your  roll no',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            width: 18,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: 'enter your email',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: 'enter your password',
+              ),
+            ),
+          ),
+      SizedBox(
+        height: 10,
+        width: 18,
+      ),
+      ElevatedButton(onPressed: () {},style: ElevatedButton.styleFrom(primary: Colors.green), child: Text('Login')),
+
+        ],
+      ),
+    );
+  }
+}
