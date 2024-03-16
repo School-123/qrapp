e11c4d50c7d6056eb01c7d00d8f8936c361231f4
[name] <abhijithm5678@gmail.com>
2024-03-16 15:28:32 +0530

    registration api

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
 
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_app/qr.dart';
+import 'package:http/http.dart' as http;
+
+import 'main.dart';
 
 class registration extends StatefulWidget {
   const registration({Key? key}) : super(key: key);
@@ -11,6 +15,41 @@ class registration extends StatefulWidget {
 }
 
 class _registrationState extends State<registration> {
+  TextEditingController name = TextEditingController();
+  TextEditingController rollno = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  void reg() async {
+    Uri uri = Uri.parse('https://scnner-web.onrender.com/api/register');
+    var response = await http.post(uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'name': name.text,
+          'roll': rollno.text,
+          'email': email.text,
+          'password': password.text,
+        }));
+    print(response.statusCode);
+    print(response.body);
+    var data = jsonDecode(response.body);
+    print(data["message"]);
+
+    if (response.statusCode == 200) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) =>  QRApp(),));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(data["message"])
+      ));
+    }
+
+    print(name.text);
+    print(rollno.text);
+    print(email.text);
+    print(password.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +68,7 @@ class _registrationState extends State<registration> {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextField(
+              controller: name,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
@@ -44,6 +84,7 @@ class _registrationState extends State<registration> {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextField(
+              controller: rollno,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
@@ -59,6 +100,7 @@ class _registrationState extends State<registration> {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextField(
+              controller: email,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
@@ -70,6 +112,7 @@ class _registrationState extends State<registration> {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextField(
+              controller: password,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
@@ -78,12 +121,18 @@ class _registrationState extends State<registration> {
               ),
             ),
           ),
-      SizedBox(
-        height: 10,
-        width: 18,
-      ),
-      ElevatedButton(onPressed: () {Navigator.push(context,MaterialPageRoute(builder:(context)=>Qrpage()));},style: ElevatedButton.styleFrom(backgroundColor: Colors.green), child: Text('Login')),
-
+          SizedBox(
+            height: 10,
+            width: 18,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                reg();
+              //   Navigator.push(
+              //       context, MaterialPageRoute(builder: (context) => Qrpage()));
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: Text('Login')),
         ],
       ),
     );
